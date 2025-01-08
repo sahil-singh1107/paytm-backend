7669f78d50c287a073c53078af45204b507aa8e3
sahil-singh1107 <sahilpython@gmail.com>
2025-01-08 20:08:45 +0530

    setup project

---
+++ src/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+class Database {
+    connection = mongoose.connection;
+
+    constructor() {
+        try {
+            this.connection
+                .on('open', console.info.bind(console, 'Database connection: open'))
+                .on('close', console.info.bind(console, 'Database connection: close'))
+                .on('disconnected', console.info.bind(console, 'Database connection: disconnecting'))
+                .on('reconnected', console.info.bind(console, 'Database connection: reconnected'))
+                .on('fullsetup', console.info.bind(console, 'Database connection: fullsetup'))
+                .on('all', console.info.bind(console, 'Database connection: all'))
+                .on('error', console.error.bind(console, 'MongoDB connection: error:'));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async connect(username: string, password: string, dbname: string) {
+        try {
+            await mongoose.connect(
+                `mongodb+srv://${username}:${password}@cluster0.2a7nn.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async close() {
+        try {
+            await this.connection.close();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+module.exports = new Database();
